d4d3cd1fdd2b5a3aef5e66d96737eea980f3674e
Bukareszt <mich.eho@onet.pl>
2021-02-13 11:57:44 +0100

    CryptoChat init

---
+++ nextjsfront/src/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header() {
+    return(
+        <nav class="navbar navbar-dark bg-dark">
+            <div className="row col-12 d-flex justify-content-center text-white">
+            <span className="h3">End-to-End Crypto Chat</span>
+            </div>
+        </nav>
+    )
+}
+export default Header;
